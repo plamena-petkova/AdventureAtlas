bc292406d6ea01fe55e75573eb8f9798a2054c36
Plamena Petkova <pl.petkova@gmail.com>
2024-07-11 16:43:49 +0300

    Added workfloe section in the landing page

--- src/components/workflow/workflow.component.ts
+++ src/components/workflow/workflow.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-workflow',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatCardModule],
   templateUrl: './workflow.component.html',
   styleUrl: './workflow.component.scss'
 })
