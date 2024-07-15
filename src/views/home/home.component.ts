2d3a4ad22be00ff7ecb5ec299cf83ea46913f4e5
Plamena Petkova <pl.petkova@gmail.com>
2024-07-15 16:48:58 +0300

    Change in the background color and footer

--- src/views/home/home.component.ts
+++ src/views/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavigationComponent } from '../../components/navigation/navigation.component';
 import { WorkflowComponent } from '../../components/workflow/workflow.component';
 import { PopularPlacesComponent } from '../../components/popular-places/popular-places.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavigationComponent, WorkflowComponent, PopularPlacesComponent],
+  imports: [NavigationComponent, WorkflowComponent, PopularPlacesComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
